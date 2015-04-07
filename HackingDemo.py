249b212e59d8f8df46554c67ab1b29c1e6d0352c
root <omertu@gmail.com>
2015-04-07 19:34:44 +0300

    test

--- HackingDemo.py
+++ HackingDemo.py
@@ -1,2 +1,18 @@
 __author__ = 'Soteria'
-ha
+
+
+import logging
+logging.getLogger('scapy')
+
+try:
+	from scapy.all import *
+except ImportError:
+	print 'This Requires Scapy To Be Installed.'
+	from sys import exit
+	exit(-1)
+
+
+def
+
+if __name__ == '__main__':
+	interact(mydict = globals(), mybanner = 'Hacking Demo')
