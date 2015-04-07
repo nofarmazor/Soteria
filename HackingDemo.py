62e273c8a7a2d6a941b9ff69804db28206ec5055
root <omertu@gmail.com>
2015-04-07 20:33:32 +0300

    test

--- HackingDemo.py
+++ HackingDemo.py
@@ -1,5 +1,15 @@
 __author__ = 'Soteria'
 
+# Sniff link-status and route request messages
+# Extract relevant sequence numbers:
+    ## IEEE sequence number
+    ## zigbee NWK sequence number
+    ## security header - Frame Counter
+# Create new packet:
+# Change sequence numbers in the new packet
+# Create new packet data
+# Encrypt the data with the key
+# Inject!
 
 import logging
 logging.getLogger('scapy')
@@ -11,8 +21,7 @@ except ImportError:
 	from sys import exit
 	exit(-1)
 
+a = Ether()/IP(dst="www.slashdot.org")/TCP()/"GET /index.html HTTP/1.0 \n\n"
+IP().show()
 
-def
-
-if __name__ == '__main__':
-	interact(mydict = globals(), mybanner = 'Hacking Demo')
+srloop(IP(dst="packetlife.net")/ICMP(), count=3)
