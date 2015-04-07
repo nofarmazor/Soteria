c1621d064a5d7de273216be258f0f33bbdeeacd0
root <omertu@gmail.com>
2015-04-07 20:43:11 +0300

    Merge branch 'master' of https://github.com/nofarmazor/Soteria
    Conflicts:
    	HackingDemo.py

--- HackingDemo.py
+++ HackingDemo.py
@@ -1,5 +1,15 @@
 __author__ = 'Soteria'
 
+# Sniff link-status and route request messages
+# Extract relevant sequence numbers:
+    ## IEEE sequence number
+    ## zigbee NWK sequence number
+    ## security header - Frame Counter
+# Create new packet:
+# Change sequence numbers in the new packet
+# Create new packet data
+# Encrypt the data with the key
+# Inject!
 
 import logging
 logging.getLogger('scapy')
@@ -11,6 +21,8 @@ except ImportError:
 	from sys import exit
 	exit(-1)
 
+a = Ether()/IP(dst="www.slashdot.org")/TCP()/"GET /index.html HTTP/1.0 \n\n"
+IP().show()
 
 def
 
